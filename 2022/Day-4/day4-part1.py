618992a52773dbe413dfe0c50dbec816bf5676f4
Qazaroth <65128305+Qazaroth@users.noreply.github.com>
2023-01-02 19:07:49 +0800

    Day 4 2022 Part 1
    Day 4 2022 Part 1 completed. Updated README.md too

--- 2022/Day-4/day4-part1.py
+++ 2022/Day-4/day4-part1.py
@@ -1,4 +1,35 @@
-inputFile = open("testinput.txt", "r")
+inputFile = open("input.txt", "r")
 inputFileData = inputFile.read().splitlines()
 
-print(inputFileData)
+def isSubset(l1, l2):
+    flag1 = False
+    flag2 = False
+
+    if (all(x in l1 for x in l2)): flag1 = True
+    if (all(x in l2 for x in l1)): flag2 = True
+
+    return flag1 or flag2
+
+def getSections(list: str):
+    boundaries = list.split("-")
+    boundary1 = int(boundaries[0])
+    boundary2 = int(boundaries[-1])
+
+    sections = []
+    
+    for i in range(boundary1, boundary2 + 1):
+        sections.append(i)
+
+    return sections
+
+fullyContained = 0
+for pair in inputFileData:
+    pairs = pair.split(",")
+
+    firstElf = getSections(pairs[0])
+    secondElf = getSections(pairs[1])
+    
+    if isSubset(firstElf, secondElf):
+        fullyContained += 1
+
+print(fullyContained)
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 **It is recommended you attempt the challenges yourself before looking through the codes!**
 
+Inputs of the challenges have been omitted starting from the commit of 2022 Day 4 onwards!
+
 Website: https://adventofcode.com/
